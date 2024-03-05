4e4d19d06c654c2203382eba6c28585beb152446
bartekaron <78853724+bartekaron@users.noreply.github.com>
2024-03-05 14:02:51 +0100

    Backend feltöltése

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
+var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : '',
+  database        : 'bevasarlolista'
+});
+
+app.use(cors());
+
+app.get('/mock_data', (req, res) => {
+  pool.query('SELECT * from mock_data', function (error, results) {
+    if (error) {
+      res.status(500).send(error);
+    }else{
+      res.status(200).send(results);
+    }
+ 
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- js/app.js
+++ js/app.js
@@ -1,84 +1,21 @@
-var app = angular.module('orarendApp', ['ngRoute', 'ngNotify']);
+var app = angular.module('bevasarlo', ['ngRoute', 'ngNotify']);
+let kategoria = document.querySelector("#kategoria");
 
-app.run(function($rootScope, $window){
-    $rootScope.title = 'Órarend Applikáció';
-    $rootScope.company = 'Bajai SZC Türr István Technikum';
-    $rootScope.author = '12.A szoftverfejlesztő';
+
+app.run(function($rootScope){
+
+    $rootScope.termekitems = [];
     
-    $rootScope.aktDay = moment().day()-1;
-    $rootScope.aktTime = moment(new Date()).format('H:mm');
+
+
+    axios.get('http://localhost:3000/mock_data').then(res => {
+        $rootScope.termekitems = res.data;
+        $rootScope.$apply();
+    }); 
+
     
-    // a JWT tokenből kiolvassuk a fehasználó adatait (token payload)
-    $rootScope.getLoggedUser = function(token){
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace('-', '+').replace('_', '/');
-        var loggedUser = JSON.parse($window.atob(base64));
-        return loggedUser;
-    }
-
-
-    if (sessionStorage.getItem('OrarendApp')){
-        $rootScope.isLoggedIn = true;
-        token = JSON.parse(sessionStorage.getItem('OrarendApp')).token;
-   //     console.log(token);
-        $rootScope.loggedUser = $rootScope.getLoggedUser(token);
-    }else{
-        $rootScope.isLoggedIn = false;
-        $rootScope.loggedUser = null;
-    }
-
-    $rootScope.days = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek"];
-    $rootScope.hours = [
-        {
-            hour: 0,
-            start: "7:15",
-            end: "7:55"
-        },
-        {
-            hour: 1,
-            start: "8:00",
-            end: "8:45"
-        }, 
-        {
-            hour: 2,
-            start: "8:55",
-            end: "9:40"
-        },
-        {
-            hour: 3,
-            start: "9:50",
-            end: "10:35"
-        },
-        {
-            hour: 4,
-            start: "10:50",
-            end: "11:35"
-        },
-        {
-            hour: 5,
-            start: "11:45",
-            end: "12:30"
-        },
-        {
-            hour: 6,
-            start: "12:40",
-            end: "13:25"
-        },
-        {
-            hour: 7,
-            start: "13:30",
-            end: "14:15"
-        },
-        {
-            hour: 8,
-            start: "14:45",
-            end: "15:25"
-        }
-    ]
-
-    $rootScope.serverUrl = 'http://sugomedia.ddns.net:2999';
-
-   
+    
+    
 });
 
 app.config(function($routeProvider){
